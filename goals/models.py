96c3fc6f3b2446c0e7e2a081d614639ebfcb60b6
Kuro <kuro@pop-os.localdomain>
2023-05-10 22:43:33 +0530

    change goal models structure

--- goals/models.py
+++ goals/models.py
@@ -2,18 +2,13 @@ from django.db import models
 
 
 class Goal(models.Model):
-    goal_type = models.CharField(max_length=50, blank=True)
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=200, blank=True, default='')
     created_at = models.DateTimeField('date published', auto_now=True)
     created_by = models.CharField(max_length=50, blank=True)
-
-
-class UserGoal(models.Model):
     user_id = models.CharField(max_length=50)
     starts_on = models.DateTimeField(null=True)
     ends_on = models.DateTimeField(null=True)
     percentage_completed = models.IntegerField(default=0)
     assigned_by = models.CharField(max_length=200, blank=True)
     status = models.CharField(max_length=50, blank=True)
-    goal = models.ForeignKey(Goal, on_delete=models.CASCADE)
--- goals/serializers.py
+++ goals/serializers.py
@@ -1,23 +1,9 @@
 from rest_framework_json_api import serializers
-from goals.models import Goal, UserGoal
-
+from goals.models import Goal
 
 class GoalSerializer(serializers.ModelSerializer):
-    included_serializers = {
-        'usergoal': "goals.serializers.UserGoalSerialier",
-    }
-
     class Meta:
         model = Goal
-        fields = ('goal_type', 'title', 'description',
-                  'created_at', 'created_by')
-
-
-class UserGoalSerialier(serializers.ModelSerializer):
-    included_serializers = {
-        'goal': GoalSerializer,
-    }
-    class Meta:
-        model = UserGoal
-        fields = ('user_id', 'starts_on', 'ends_on',
-                  'percentage_completed', 'assigned_by', 'status', 'goal')
+        fields = ('title', 'description',
+                  'created_at', 'created_by', 'user_id', 'starts_on', 
+                  'ends_on', 'percentage_completed', 'assigned_by', 'status')
