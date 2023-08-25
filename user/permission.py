35d79a0d0a8662bc001ddc337ad14ced64576677
Zovio <zovio@pop-os.localdomain>
2023-08-25 16:42:25 +0530

    fix rest key permission class issue

--- user/permission.py
+++ user/permission.py
@@ -3,6 +3,5 @@ from .utils import env
 
 class RestKeyPermission(permissions.BasePermission):
     def has_permission(self, request, view):
-        key = request.META.get('REST_KEY')  # Replace with your actual header key name
-        # Check if the key is correct (you can replace 'YOUR_CORRECT_KEY' with your actual key)
+        key = request.headers.get('Rest-Key')
         return key == env('REST_KEY')
